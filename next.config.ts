b71c61a132cf9654579a181a3dd86a5f651fbfc0
Vision <vision@users.noreply.github.com>
2026-02-09 21:29:40 +0000

    Configure basePath for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  basePath: "/brandxploited-myversion",
   images: {
     unoptimized: true,
   },
